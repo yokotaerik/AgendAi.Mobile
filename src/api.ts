71d4dc8f5f5c42a434b0e4585162b090c6662c9b
yokotaerik <yokota082003@gmail.com>
2025-05-07 21:35:20 -0300

    rodandoooo

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import Toast from "react-native-toast-message";
 
 export const baseURL =
   Platform.OS == "android"
-    ? "http://192.168.15.2:5000/api"
+    ? "http://192.168.40.172:5000/api"
     : "http://localhost:5000/api";
 
 const api = axios.create({
